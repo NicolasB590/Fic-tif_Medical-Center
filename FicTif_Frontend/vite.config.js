86073686c6442d7600647aec90295a01368d8fa6
NicolasB590 <nicolas.bernard590@gmail.com>
2025-01-03 14:31:28 +0100

    fix redirection

--- FicTif_Frontend/vite.config.js
+++ FicTif_Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://fic-tif-medical-center.onrender.com",
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ""),
       },
